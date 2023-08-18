e2b719aea369310f5dde1410d910a05cb44ae4a3
JScof1 <jacob.scofield@auticon.us>
2023-08-18 10:23:08 -0600

    Created outline

--- Spaceship_Pilots.bat
+++ Spaceship_Pilots.bat
@@ -1,4 +1,20 @@
 @ECHO OFF
+
 :: test that i can run a batch file
-ECHO Hello World
+:: ECHO Hello World
+
+:: Start up an alpine:latest docker
+
+:: give it python
+
+:: give it swapi-python
+
+:: Get all spaceships
+
+:: Check the spaceships for missing pilots, remove those
+
+:: Print only spaceship name, and pilot list
+
+:: clean up docker
+
 PAUSE
